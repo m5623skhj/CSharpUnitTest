eacefa4eefc9232f16ee76279909a2f5ca3b39e4
DESKTOP-51V9NP0\김형진 <retemptt12@gmail.com>
2023-07-27 23:05:48 +0900

    * 이전에 잘못 만들어서 삭제하고 다시 추가; * UnitTest 추가

---
+++ CSharpUnitTest/CSharpUnitTest/Add.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpUnitTest
+{
+    public class Test_Add
+    {
+        public readonly string name = "Add";
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ CSharpUnitTest/TestProject/AddTest.cs
@@ -0,0 +1,18 @@
+﻿using CSharpUnitTest;
+
+namespace UnitTestProject
+{
+    public class AddTest
+    {
+        Test_Add test = new Test_Add();
+
+        [Fact]
+        public void Add()
+        {
+            Assert.NotNull(test);
+
+            Assert.Equal("Add", test.name);
+            Assert.Equal(300, test.Add(100, 200));
+        }
+    }
+}
