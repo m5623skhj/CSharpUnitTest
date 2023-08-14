6e33a7166b9008cebbff872d2fbb08f0c976f942
m5623skhj <m5623skhj@gmail.com>
2023-08-14 22:36:29 +0900

    * ClassFixture, GlobalFixture 추가

--- CSharpUnitTest/TestProject/AddTest.cs
+++ CSharpUnitTest/TestProject/AddTest.cs
@@ -1,16 +1,47 @@
 ﻿namespace UnitTestProject
 {
-    public class AddTest
+    public class AddTestFixture : IDisposable
     {
+        public AddTestFixture()
+        {
+            Console.WriteLine("AddTestFixture constructor()");
+        }
+
+        public void Dispose() 
+        {
+            Console.WriteLine("AddTestFixture Dispose()");
+        }
+    }
+
+    [Collection("GlobalFixture")]
+    public class AddTest : IDisposable, IClassFixture<AddTestFixture>
+    {
+        public AddTest()
+        {
+            Console.WriteLine("AddTest constructor()");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("AddTest Dispose()");
+        }
+
         Test_Add test = new Test_Add();
 
         [Fact]
         public void Add()
         {
+            Console.WriteLine("Call Add()");
             Assert.NotNull(test);
 
             Assert.Equal("Add", test.name);
             Assert.Equal(300, test.Add(100, 200));
+        }
+
+        [Fact]
+        public void Temp()
+        {
+            Console.WriteLine("Call temp()");
         }
     }
 }
---
+++ CSharpUnitTest/TestProject/GlobalFixture.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class GlobalFixture : IDisposable
+    {
+        public GlobalFixture()
+        {
+            Console.WriteLine("GlobalFixture : constructor()");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("GlobalFixture : Dispose()");
+        }
+    }
+
+    // 더미 클래스
+    [CollectionDefinition("GlobalFixture")]
+    public class GlobalFixtureCollection : ICollectionFixture<GlobalFixture>
+    {
+    }
+}
